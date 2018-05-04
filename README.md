1ded9c68ec80028e966c17d45b7442c758723fa5
Calle <36276161+qalleh1@users.noreply.github.com>
2018-05-04 19:54:55 +0200

    update
    major cleanup and added teleports

---
+++ README.md
@@ -0,0 +1,8 @@
+# esx_carprice
+
+Requirements:
+esx_vehicleshop
+
+1. Drag into resources
+2. Add start esx_carprice to server.cfg
+3. Done!
---
+++ config.lua
@@ -0,0 +1,28 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MarkerColor                = { r = 255, g = 255, b = 255 }
+--language currently available EN and SV
+Config.Locale                     = 'sv'
+
+Config.Zones = {
+
+  Katalog = {
+    Pos     = { x = 228.18 , y = -995.48, z = -99.96 },
+    Size    = { x = 1.5, y = 1.5, z = 1.0 },
+    Heading = 177.28,
+    Type    = 27,
+  },
+
+  GoDownFrom = {
+    Pos   = { x = -50.03, y = -1089.18, z = 25.48 },
+    Size  = { x = 1.5, y = 1.5, z = 1.0 },
+    Type  = 27,
+  },
+
+  GoUpFrom = {
+    Pos   = { x = 240.98, y = -1004.85, z = -99.98 },
+    Size  = { x = 1.5, y = 1.5, z = 1.0 },
+    Type  = 27,
+  },
+
+}
