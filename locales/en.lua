8dbdbdec610ddccc29979eecdf3ed6859255160b
JonarSonaer <haikara98@gmail.com>
2022-02-17 16:29:33 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+Locales['en'] = {
+  -- Everything
+  ['press_e'] = 'Press ~INPUT_CONTEXT~ to open the menu',
+  ['costs'] = ' Costs: ',
+  ['currency'] = ' $',
+  ['back'] = 'Go back',
+  ['contact_dealer'] = 'Contact the dealer, it costs: ',
+}
