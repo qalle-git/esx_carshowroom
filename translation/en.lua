f1746801ba9659dbda4fa59814dc2e6225847032
JonarSonaer <haikara98@gmail.com>
2022-02-17 16:29:18 +0100

    Delete translation directory

--- translation/en.lua
+++
@@ -1,8 +0,0 @@
-Locales['en'] = {
-  -- Everything
-  ['press_e'] = 'Press ~INPUT_CONTEXT~ to open the menu',
-  ['costs'] = ' Costs: ',
-  ['currency'] = ' $',
-  ['back'] = 'Go back',
-  ['contact_dealer'] = 'Contact the dealer, it costs: ',
-}
